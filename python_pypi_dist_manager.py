85c17133c296dbc509d22e064c26913d829588db
Shlomi Fish <shlomif@shlomifish.org>
2021-12-18 09:00:46 +0200

    Convert to pydistman's filter_test_reqs option.

--- python_pypi_dist_manager.py
+++ python_pypi_dist_manager.py
@@ -14,57 +14,7 @@ from pydistman import DistManager
 
 class Derived(DistManager):
     """docstring for Derived"""
-
-    def _build_only_command_custom_steps(self):
-        req_bn = "requirements.txt"
-        req_fn = "{src_dir}/" + req_bn
-        with open(self._myformat("{dest_dir}/tox.ini"), "wt") as ofh:
-            ofh.write(
-                "[tox]\nenvlist = py39\n\n" +  ## noqa
-                "[testenv]\ndeps =" + "".join(
-                    ["\n\t" + x for x in
-                     sorted(
-                         list(self._testreqs.keys()) +  ## noqa
-                         self._fmt_slurp(req_fn).split("\n"))]
-                ) + "\n" + ## noqa
-                "\ncommands = pytest\n")
-        return
-
-    def _reqs_mutate(self, fn_proto):
-        fn = self._myformat(fn_proto)
-        txt = self._slurp(fn)
-        d = {}
-        testreqs = {}
-        for line in txt.split("\n"):
-            if 0 == len(line):
-                continue
-            m = re.match("\\A([A-Za-z0-9_\\-]+)>=([0-9\\.]+)\\Z", line)
-            if m:
-                req = m.group(1)
-                ver = m.group(2)
-            else:
-                req = line
-                ver = '0'
-            if req in ['coverage', 'pytest', 'pytest-cov', 'requests',
-                       'twine', ]:
-                testreqs[req] = '0'
-                continue
-            if ver == '0':
-                if req not in d:
-                    d[req] = '0'
-            else:
-                if req not in d or d[req] == '0':
-                    d[req] = ver
-                else:
-                    raise BaseException(
-                        "mismatch reqs: {} {} {}".format(req, ver, d[req]))
-        self._testreqs = testreqs
-        self._reqs = list(sorted([
-            x + ('' if v == '0' else '>=' + v) for x, v in d.items()]))
-        txt = "".join([x + "\n" for x in self._reqs])
-        with open(fn, "wt") as ofh:
-            ofh.write(txt)
-
+    pass
 
 try:
     cmd = sys.argv.pop(1)
@@ -84,5 +34,6 @@ obj = Derived(
     project_email="shlomif@cpan.org",
     full_name="Shlomi Fish",
     github_username="shlomif",
+    filter_test_reqs=True,
 )
 obj.run_command(cmd=cmd, args=[])
