c46225b659b58fba49d03c0ae65eca538f3de2d7
Shlomi Fish <shlomif@shlomifish.org>
2024-01-04 10:15:32 +0200

    add tests

--- code/tests/test_pysol_cards.py
+++ code/tests/test_pysol_cards.py
@@ -218,3 +218,40 @@ KD AC QH 6S 3D 3C
 """,
             "pysolfc binary_star deal #240000",
         )
+
+    def test_pysolfc_black_hole(self):
+        from pysol_cards.cards import CardRenderer
+        from pysol_cards.deal_game import Game
+        from pysol_cards.random_base import RandomBase
+        deal_s = Game(
+            game_id="black_hole",
+            game_num=240000,
+            which_deals=RandomBase.DEALS_PYSOLFC,
+            max_rank=13
+        ).calc_layout_string(
+            CardRenderer(print_ts=True)
+        )
+        # make_pysol_freecell_board.py -F -t 240000 black_hole
+        self.assertEqual(
+            deal_s,
+            """Foundations: AS
+3S 8H JH
+7H 9C 5C
+6H 5H 8S
+5S 2S 3C
+8D 3H 4H
+5D TD AH
+KC 3D 7S
+4S QC QH
+TC AC 2C
+6C 9H 4D
+TH JD KS
+JC 7C 4C
+QS AD 2H
+9D 6D 8C
+7D KD JS
+2D 9S TS
+6S QD KH
+""",
+            "pysolfc black_hole deal #240000",
+        )
