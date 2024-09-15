b73ad23534b4dc5089981f15893cd94acfcc7476
Shlomi Fish <shlomif@shlomifish.org>
2024-09-15 06:59:46 +0300

    clean-up pydistman/cookiecutter fluff before PyPI

--- python_pypi_dist_manager.py
+++ python_pypi_dist_manager.py
@@ -6,12 +6,32 @@
 #
 # Distributed under the terms of the MIT license.
 
+
+import os
+import os.path
+import shutil
+
 from pydistman import DistManager
 
 
 class Derived(DistManager):
     """docstring for Derived"""
-    pass
+    def _fmt_unlink(self, fn_proto):
+        """rmtree the formatted fn_proto if it exists."""
+        fn = self._myformat(fn_proto)
+        if os.path.exists(fn):
+            os.unlink(fn)
+
+    def _build_only_command_custom_steps(self):
+        # Fix/workaround for https://github.com/shlomif/pysol_cards/issues/9
+        #
+        # This issue may probably affect other pydistman-based distributions:
+        #
+        # https://github.com/shlomif/pydistman
+        self._fmt_unlink("{dest_dir}/pysol_cards/__main__.py")
+        self._fmt_unlink("{dest_dir}/pysol_cards/template.py")
+        self._fmt_unlink("{dest_dir}/tests/__init__.py")
+        self._fmt_unlink("{dest_dir}/tests/test_sanity.py")
 
 
 dist_name = "pysol_cards"
